da3e68b6f07e167087e0f93f6036444ec7e78d6e
qijunyao <qijunyao@xiaohongshu.com>
2020-09-28 15:16:47 +0800

    append

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version ("1.0.10.RELEASE")
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
-    `maven`
+    `maven-publish`
 }
 
 group = "org.yqj.boot"
@@ -16,25 +16,19 @@ java {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
 
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.projectlombok:lombok")
     implementation("com.google.guava:guava:22.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-
-    testCompileOnly("org.projectlombok:lombok")
-    testAnnotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude("org.junit.vintage", "junit-vintage-engine")
